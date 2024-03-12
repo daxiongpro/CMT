486623682629ba1af2ae298f40e8a639625d6c81
daxiongpro <zhengyi.cai@geely.com>
2024-03-12 09:24:55 +0800

    测试时，从验证集改为测试集

--- tools/bash_runner/test.sh
+++ tools/bash_runner/test.sh
@@ -4,5 +4,9 @@ CONFIG_FILE='projects/configs/fusion/cmt_voxel0075_vov_1600x640_cbgs.py'
 PTH='ckpt/voxel0075_vov_1600x640_epoch20.pth'
 DEBUG_PY='-m debugpy --listen 8531 --wait-for-client'
 
-python ${TEST_PY} ${CONFIG_FILE} ${PTH} --eval bbox
+# 测试 mAP 等指标
+# python ${TEST_PY} ${CONFIG_FILE} ${PTH} --eval bbox
 # python ${DEBUG_PY} ${TEST_PY} ${CONFIG_FILE} ${PTH} --eval bbox
+
+# 测试 nuscenes 测试集，并保存 nuscenes 格式结果
+python ${TEST_PY} ${CONFIG_FILE} ${PTH} --format-only --eval-options jsonfile_prefix=test/
