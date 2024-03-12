fd59d46a6ef076b90855ad81b0655cf3ada8af1e
zhengyi.cai <zhengyi.cai@geely.com>
2024-03-12 11:07:41 +0800

    多块显卡测试

--- tools/bash_runner/test.sh
+++ tools/bash_runner/test.sh
@@ -3,10 +3,11 @@ TEST_PY='tools/test.py'
 CONFIG_FILE='projects/configs/fusion/cmt_voxel0075_vov_1600x640_cbgs.py'
 PTH='ckpt/voxel0075_vov_1600x640_epoch20.pth'
 DEBUG_PY='-m debugpy --listen 8531 --wait-for-client'
+GPU_NUM=1
 
 # 测试 mAP 等指标
 # python ${TEST_PY} ${CONFIG_FILE} ${PTH} --eval bbox
 # python ${DEBUG_PY} ${TEST_PY} ${CONFIG_FILE} ${PTH} --eval bbox
 
-# 测试 nuscenes 测试集，并保存 nuscenes 格式结果
-python ${TEST_PY} ${CONFIG_FILE} ${PTH} --format-only --eval-options jsonfile_prefix=test/
+# 多块显卡测试
+tools/dist_test.sh ${CONFIG_FILE} ${PTH} ${GPU_NUM} --format-only --eval-options jsonfile_prefix=test/
