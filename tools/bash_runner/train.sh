88489638a007c4f88cb4e3a983ee04a2954d7085
zhengyi.cai <zhengyi.cai@geely.com>
2024-03-13 09:02:18 +0800

    多卡训练

--- tools/bash_runner/train.sh
+++ tools/bash_runner/train.sh
@@ -3,6 +3,11 @@ TRAIN_PY='tools/train.py'
 CONFIG_FILE='projects/configs/fusion/cmt_voxel0075_vov_1600x640_cbgs.py'
 WORK_DIR="runs/${DATE}/"
 DEBUG_PY='-m debugpy --listen 8531 --wait-for-client'
+GPU_NUM=8
 
-python ${TRAIN_PY} ${CONFIG_FILE}
+# 单卡训练
+# python ${TRAIN_PY} ${CONFIG_FILE}
 # python ${DEBUG_PY} ${TRAIN_PY} ${CONFIG_FILE}
+
+# 多卡训练
+bash tools/dist_train.sh ${CONFIG_FILE} ${GPU_NUM}
