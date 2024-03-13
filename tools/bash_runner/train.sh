3ee55118eed46e518a921345a75e11aeb1a2df7b
zhengyi.cai <zhengyi.cai@geely.com>
2024-03-13 14:57:28 +0800

    use resnet as backbone

--- tools/bash_runner/train.sh
+++ tools/bash_runner/train.sh
@@ -1,13 +1,9 @@
-DATE=$(date '+%Y-%m-%d_%H-%M-%S')
 TRAIN_PY='tools/train.py'
-CONFIG_FILE='projects/configs/fusion/cmt_voxel0075_vov_1600x640_cbgs.py'
-WORK_DIR="runs/${DATE}/"
+# CONFIG_FILE='projects/configs/fusion/cmt_voxel0075_vov_1600x640_cbgs.py'  # OOM on RTX 3090 with batch_size=2
+CONFIG_FILE='projects/configs/fusion/cmt_voxel0100_r50_800x320_cbgs.py'
 DEBUG_PY='-m debugpy --listen 8531 --wait-for-client'
 GPU_NUM=8
 
-# 单卡训练
-# python ${TRAIN_PY} ${CONFIG_FILE}
-# python ${DEBUG_PY} ${TRAIN_PY} ${CONFIG_FILE}
 
 # 多卡训练
 bash tools/dist_train.sh ${CONFIG_FILE} ${GPU_NUM}
