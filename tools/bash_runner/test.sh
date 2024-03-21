520cd75d9accc63d89bb5058765be88ac41b0305
daxiongpro <910660298@qq.com>
2024-03-21 16:25:29 +0800

    默认单块显卡测试

--- tools/bash_runner/test.sh
+++ tools/bash_runner/test.sh
@@ -5,9 +5,10 @@ PTH='ckpts/voxel0075_vov_1600x640_epoch20.pth'
 DEBUG_PY='-m debugpy --listen 8531 --wait-for-client'
 GPU_NUM=1
 
-# 测试 mAP 等指标
-# python ${TEST_PY} ${CONFIG_FILE} ${PTH} --eval bbox
+# 单块显卡测试
+python ${TEST_PY} ${CONFIG_FILE} ${PTH} --eval bbox
 # python ${DEBUG_PY} ${TEST_PY} ${CONFIG_FILE} ${PTH} --eval bbox
 
 # 多块显卡测试
-tools/dist_test.sh ${CONFIG_FILE} ${PTH} ${GPU_NUM} --format-only --eval-options jsonfile_prefix=test/
+# tools/dist_test.sh ${CONFIG_FILE} ${PTH} ${GPU_NUM} --eval bbox
+# tools/dist_test.sh ${CONFIG_FILE} ${PTH} ${GPU_NUM} --format-only --eval-options jsonfile_prefix=test/
