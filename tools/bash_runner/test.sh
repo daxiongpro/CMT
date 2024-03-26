771c567bc5a6efd828fe7281889ac2157034082e
zhengyi.cai <zhengyi.cai@geely.com>
2024-03-26 13:55:22 +0800

    GPU_NUM=8

--- tools/bash_runner/test.sh
+++ tools/bash_runner/test.sh
@@ -3,7 +3,7 @@ TEST_PY='tools/test.py'
 CONFIG_FILE='projects/configs/fusion/cmt_voxel0075_vov_1600x640_cbgs.py'
 PTH='ckpts/voxel0075_vov_1600x640_epoch20.pth'
 DEBUG_PY='-m debugpy --listen 8531 --wait-for-client'
-GPU_NUM=1
+GPU_NUM=8
 
 # 单块显卡测试
 python ${TEST_PY} ${CONFIG_FILE} ${PTH} --eval bbox
